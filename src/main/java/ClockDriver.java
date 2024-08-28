771ced88b080e8b9f6273b2fe5328a9a997d5149
Benjamin Johnson <bjohnson@fcusd.org>
2024-08-28 16:08:31 -0700

    Update ClockDriver.java

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -3,30 +3,40 @@ public class ClockDriver
 {
     public static void main(String[] args){
         //Implement tests for your ClockDisplay
-        //Test that you can do the basics
-        //  * Empty constructor & read time
-        //  * Constructor w/ time given & read time
-        //  * Set time
-        //  * Read time
-        //  * Tick time
+        //  * Test that you can do the basics
+        //      * Empty constructor & read time
+        //      * Constructor w/ time given & read time
+        //      * Set time
+        //      * Read time
+        //      * Tick time
+        //  * Tick test for 03:32 to 03:33
+        //  * Tick test for 03:09 to 03:10
+        //  * Tick test for 01:59 to 02:00
+        //  * Tick test for 09:59 to 10:00
+        //  * Tick test for 23:59 to 00:00
         
         
         //Implement tests for your ClockDisplaySeconds
-        //Test that you can do the basics
-        //  * Empty constructor & read time
-        //  * Constructor w/ time given & read time
-        //  * Set time
-        //  * Read time
-        //  * Tick time
+        //  * Test that you can do the basics
+        //      * Empty constructor & read time
+        //      * Constructor w/ time given & read time
+        //      * Set time
+        //      * Read time
+        //      * Tick time
+        //  * Tick test for 03:32:59 to 03:33:00
+        //  * Tick test for 01:00:59 to 01:01:00
+        //  * Tick test for 01:59:59 to 02:00:00
+        //  * Tick test for 23:59:59 to 00:00:00
         
         /*CHALLENGE*/
         //Implement tests for your ClockDisplay12Hour
-        //Test that you can do the basics
-        //  * Empty constructor & read time
-        //  * Constructor w/ time given & read time
-        //  * Set time
-        //  * Read time
-        //  * Tick time
+        //  * Test that you can do the basics
+        //      * Empty constructor & read time
+        //      * Constructor w/ time given & read time
+        //      * Set time
+        //      * Read time
+        //      * Tick time
+        //  * Tick test for 03:32PM to 03:33PM
         //  * Tick test for 11:59PM to 12:00AM
         //  * Tick test for 11:59AM to 12:00PM
         //  * Tick test for 12:59PM to 01:00PM
